86fec302fd38948780d484ee488a8bbbebc7b67c
Gunter Zeilinger <gunterze@gmail.com>
2017-08-15 12:16:40 +0200

    Use Standalone Keycloak server instead Wildfly Keycloak Overlay dcm4che/dcm4chee-arc-light#916

--- configuration/adjust-dcm4che-realm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sed -e "s%dc=dcm4che,dc=org%${LDAP_BASE_DN}%" \
-    -e "s%ldap://ldap:389%ldap://${LDAP_HOST}:${LDAP_PORT}%" \
-    -e "s%\"bindCredential\" : \[ \"secret\"%\"bindCredential\" : \[ \"${LDAP_ROOTPASS}\"%" \
-    -e "s%\"sslRequired\" : \"external\"%\"sslRequired\" : \"${SSL_REQUIRED}\"%" \
-    -i $JBOSS_HOME/standalone/configuration/dcm4che-realm.json
